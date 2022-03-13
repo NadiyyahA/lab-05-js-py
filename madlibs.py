8405d2e690c7ba96536ec4519e40c1e0a578b164
Nadiyyah Ahmed <99215113+NadiyyahA@users.noreply.github.com>
2022-03-12 23:00:54 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+ noun = 'girl'
+ adjective = 'funny'
+ adverb = 'curiously'
+ verb = 'argue'
+ print("On + a + tropical + str(noun))
+ print(Underneath + a + str(adjective) + moon)
+ print(Hangin + with + the + hula + str(adverb))
+ print(str(verb) + cause + they + all + got + answers)
+    
